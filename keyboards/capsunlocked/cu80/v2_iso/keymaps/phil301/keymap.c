e4fcea717f63e3fda2cf1cf51b709a2d8deb90e2
phil301 <phil.m.jones88@gmail.com>
2021-02-22 22:12:48 +0000

    Add tap F12 mute, Teams Ctrl+Shft+M mic mute

--- keyboards/capsunlocked/cu80/v2_iso/keymaps/phil301/keymap.c
+++ keyboards/capsunlocked/cu80/v2_iso/keymaps/phil301/keymap.c
@@ -38,17 +38,21 @@ enum layers {
 };
 
 enum tap_dancing {
-  TD_MUTE_CAD,
+  // TD_MUTE_CAD,
+  TD_MICMUTE_CAD,
+  TD_F12_MUTE,
 };
 
 // Tap dance setup
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_MUTE_CAD] = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, LCTL(LALT(KC_DEL))),
+  // [TD_MUTE_CAD] = ACTION_TAP_DANCE_DOUBLE(KC_MUTE, LCTL(LALT(KC_DEL))),
+  [TD_MICMUTE_CAD] = ACTION_TAP_DANCE_DOUBLE(LCTL(LSFT(KC_M)), LCTL(LALT(KC_DEL))),
+  [TD_F12_MUTE] = ACTION_TAP_DANCE_DOUBLE(KC_F12, KC_MUTE),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_DEFAULT] = LAYOUT_tkl_iso(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                  KC_VOLD, KC_VOLU, TD(TD_MUTE_CAD),
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  TD(TD_F12_MUTE),         KC_VOLD, KC_VOLU, TD(TD_MICMUTE_CAD),
 
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS,  KC_EQL,    KC_BSPC, KC_INS,  KC_HOME, KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC,  KC_RBRC,            KC_DEL,  KC_END,  KC_PGDN,
